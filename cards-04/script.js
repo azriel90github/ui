264f66b417ed4b29ed26ecdaa1c2f78fd63cfa13
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-10-13 15:15:25 +0100

    Update script.js

--- cards-04/script.js
+++ cards-04/script.js
@@ -1,5 +1,6 @@
 const likeButtons = document.querySelectorAll(".card__btn");
 
+// biome-ignore lint/complexity/noForEach: <explanation>
 likeButtons.forEach((likeButton) => {
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__btn--like");
